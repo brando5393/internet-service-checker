1d4ed9829998815e2ef49f31d7a2fa3d1dbb17ec
Brandon Williams <brando5393@gmail.com>
2023-03-23 00:16:45 -0500

    made major edits to program.cs and fixed some errors

--- Program.cs
+++ Program.cs
@@ -1,137 +1,111 @@
 ﻿using System;
 using System.IO;
 using System.Net.NetworkInformation;
-//using System.Timers;
+using System.Net;
 
 namespace internet_service_checker
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            // Set the console title
             Console.Title = "Internet Service Checker";
 
-
-            //Console.Write("/**/");
+            // Display the welcome message and menu options
             Console.WriteLine("===Welcome to the Internet Service Checker===");
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("===MENU===");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("1. Initialize Checker");
             Console.WriteLine("2. About This Program");
             Console.WriteLine("3. Exit");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.Write("Please enter a number: ");
             string ans = Console.ReadLine();
 
+            // Process user input using a switch statement
             switch (ans)
             {
                 case "1":
-                    // Console.Write("selected option 1");
-                    checkService();
+                    CheckService();
                     break;
-
                 case "2":
-                    Console.Write("selected option 2");
-                    // method call here
+                    // Display information about the program and return to the main menu
+                    Console.WriteLine("This program checks internet service by sending ping requests to a target IP address.");
+                    Console.WriteLine("The program will continue sending ping requests until you choose to exit.");
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to return to the main menu.");
+                    Console.ReadKey();
+                    Main(args);
                     break;
-
                 case "3":
+                    // Exit the program
                     Environment.Exit(0);
                     break;
-
                 default:
-                    Console.Write("Please enter a valid number.");
-                    break;   
+                    // Display an error message for invalid input and return to the main menu
+                    Console.WriteLine("Please enter a valid number.");
+                    Main(args);
+                    break;
             }
         }
-        public static void checkService()
+
+        static void CheckService()
         {
+            var userHomeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            // Get the current date to use in the log file name
             var currentDate = DateTime.Now.ToString("ddd M\\-dd\\-yy");
-           
+            // Initialize a variable to keep track of the number of ping attempts
             int pingNum = 0;
-            string fileName = @"C:\internet-service-log " + currentDate + ".txt";
+            // Set the file name for the log file
+            string fileName = Path.Combine(userHomeDirectory,"internet-service-log-{currentDate}.txt");
+            // Set a message to display when exiting to the main menu
             string exitMessage = "Press ESC to exit to main menu.";
-            
-            
-            
-            // Check if a file with the same name exists if so, delete it
+            // Set the target IP address for the ping requests (Google DNS server)
+            string targetIpAddress = "8.8.8.8";
+
+            // If a log file with the same name already exists, delete it
             if (File.Exists(fileName))
             {
                 File.Delete(fileName);
             }
-            // Create log file
-            Console.WriteLine("");
+
+            // Display a message indicating that the program is checking internet service
+            Console.WriteLine();
             Console.WriteLine("===STARTED CHECKING INTERNET SERVICE===");
-            Console.WriteLine("");
-            Console.WriteLine("Log file created in C drive");
+            Console.WriteLine();
+            // Display the file name for the log file
+            Console.WriteLine($"Log file created in current user's home directory: {fileName}");
+            // Create a StreamWriter object to write to the log file
             using (StreamWriter recorder = new StreamWriter(fileName))
             {
-                recorder.WriteLine("===INTERNET SERVICE LOG FOR " + currentDate + "===");
-                recorder.WriteLine("");
-                Ping serviceChecker = new Ping();
-                // create timer to send ping to google every 5 minutes
+                // Write a header to the log file with the current date
+                recorder.WriteLine($"===INTERNET SERVICE LOG FOR {currentDate}===");
+                recorder.WriteLine();
+
+                // Create a Ping object to send ping requests
+                var pingSender = new Ping();
+
+                // Set the ping options to prevent fragmentation
+                var pingOptions = new PingOptions
+                {
+                    DontFragment = true
+                };
+
+                // Set the buffer size and timeout for the ping requests
+                var buffer = new byte[32];
+                var timeout = 1000;
+
+                // Set up a timer to send ping requests every 5 minutes
                 var mainTimer = new System.Timers.Timer();
                 mainTimer.Interval = 300000;
                 mainTimer.Enabled = true;
                 mainTimer.AutoReset = true;
 
-                while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
-                {
-                    mainTimer.Elapsed += onTimedEvent;
-                    void onTimedEvent(object source, System.Timers.ElapsedEventArgs elapsedEventArgs)
-                    {
-                        try
-                        {
-                            Console.WriteLine("!Sending ping attempt to google.com");
-                            PingReply reply = serviceChecker.Send("172.217.1.132", 1000);
-                            if (reply != null)
-                            {
-                                var currentTime = DateTime.Now.ToString("hh:mm tt");
-                                string successMessage = "#PING " + pingNum + ": Status Return = SUCCESSFULL / Time Recorded = [" + currentTime + "]";
-                                pingNum++;
-                                Console.WriteLine(successMessage);
-                                recorder.WriteLine(successMessage);
-                                Console.WriteLine(exitMessage);
-                                //mainTimer.Stop();
-                                //mainTimer.Start();
-                            }
-                            else
-                            {
-                                var currentTime = DateTime.Now.ToString("hh:mm tt");
-                                string failMessage = "#PING " + pingNum + ": Status Return = FAILURE / Time Recorded = [" + currentTime + "]";
-                                pingNum++;
-                                Console.WriteLine(failMessage);
-                                recorder.WriteLine(failMessage);
-                                Console.WriteLine(exitMessage);
-                                //mainTimer.Stop();
-                                //mainTimer.Start();
-                            }
-                        }
-                        catch
-                        {
-                            Console.WriteLine("!ERROR ENCOUNTERED WHILE ATTEMPTING TO CONTACT GOOGLE SERVER");
-                            Console.WriteLine("!PRESS ANY KEY TO RETURN TO THE MAIN MENU");
-                            Console.ReadKey();
-                            Environment.Exit(0);
+                // Keep sending ping requests until the user chooses to exit
 
-                        }
-                        
-                       
-
-                    }
-                    
-                    
-                    
-                }
-                
-                
-                
-                    
-                
             }
-            
         }
     }
 }
